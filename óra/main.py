b589ba5d7f1cfd441e25bc839fa1ddd75b786322
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-12-02 15:23:03 +0100

    Órai

---
+++ óra/main.py
@@ -0,0 +1,48 @@
+neved = input("Kérem a neved: ") # alapvetően az input mindig mindent string-ként kezel
+
+print(f"Téged {neved} hívnak.")
+
+eletkorod = int(input("Kérem az életkorod: "))
+
+print(f"Téged {hívnak} és {eletkorod} vagy.")
+print(type(eletkorod))
+
+ureslista = [] # Üres lista
+
+prinnt(type(lista))
+
+lista = [17, 5, 6, 23, 75, "Lajos"] # A típusok keveredhetnek
+lista2 = [1, 2, 3, 4]
+print(f"A lista elemei: {lista}")
+
+print(f"A lista második eleme {lista[2]}")
+
+# -----------------
+
+lista.append("Kívánok!") # Lista végére teszünk egy elemet
+lista.insert(1, "20. ") # Az egyes indexre beteszi a megadott elemet.
+lista.append(lista2) # A teljes listát beszúrja a lista utolsó indexére.
+lista.extend(lista2) # A lista elemeit egyesével helyezi el az indexen
+lista.remove(4) # Megadott elemet távolítja el.
+lista.remove(lista2) # A lista elemeit egyesével helyezi el-
+lista.pop(4) # a megadott indexet törli
+lista.clear() # Törli a lista elemeit, üres listát csinál.
+lista.sort() # Rendezés növekvő sorrendbe
+lista.sort(reverse=True) # Rendezés csökkenő sorrendbe.
+
+for i in range(10):  # i = 0; i < 10; i++
+    print(i)
+for i in range(1, 10):  # i = 1; i < 10; i++
+    print(i)
+for i in range(3, 15, 2):  # i = 3; i < 5; i++
+    print(i)
+for i in lista3:  # Az i felveszi a listának az elemit
+    print(i)
+
+stringmen = "Ez egy string"
+
+for i in stringmen:  # Betünként sortöréssel kiírja a mondatot
+    print(i)
+
+print(stringmen[5])
+print(sttringmen[1:6])
